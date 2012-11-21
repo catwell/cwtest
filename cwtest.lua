98dd96c1a059bfff5fbc8893934d6dfb026a6361
Pierre Chapuis <catwell@archlinux.us>
2012-11-21 19:36:04 +0100

    verbose mode and assertions

--- cwtest.lua
+++ cwtest.lua
@@ -6,11 +6,43 @@ local printf = function(p,...)
   io.stdout:write(string.format(p,...)); io.stdout:flush()
 end
 
+local pass_assertion = function(self)
+  printf(".")
+  local info = debug.getinfo(3)
+  self.successes[#self.successes+1] = string.format(
+    "\n[OK] %s line %d (assertion)\n",
+    info.short_src,
+    info.currentline
+  )
+end
+
+local fail_assertion = function(self)
+  printf("x")
+  local info = debug.getinfo(3)
+  self.failures[#self.failures+1] = string.format(
+    "\n[KO] %s line %d (assertion)\n",
+    info.short_src,
+    info.currentline
+  )
+end
+
+local pass_eq = function(self,x,y)
+  printf(".")
+  local info = debug.getinfo(3)
+  self.successes[#self.successes+1] = string.format(
+    "\n[OK] %s line %d\n  expected: %s\n       got: %s\n",
+    info.short_src,
+    info.currentline,
+    pretty.write(y,""),
+    pretty.write(x,"")
+  )
+end
+
 local fail_eq = function(self,x,y)
   printf("x")
   local info = debug.getinfo(3)
   self.failures[#self.failures+1] = string.format(
-    "\n[FAIL] %s line %d\n  expected: %s\n       got: %s\n",
+    "\n[KO] %s line %d\n  expected: %s\n       got: %s\n",
     info.short_src,
     info.currentline,
     pretty.write(y,""),
@@ -19,33 +51,42 @@ local fail_eq = function(self,x,y)
 end
 
 local start = function(self,s)
-  assert((not self.failures),"test already started")
-  self.failures = {}
+  assert((not (self.failures or self.successes)),"test already started")
+  self.failures,self.successes = {},{}
   printf("%s ",s)
 end
 
 local done = function(self)
-  local f = self.failures
-  assert(f,"call start before done")
+  local f,s = self.failures,self.successes
+  assert((f and s),"call start before done")
   if #f > 0 then
     print(" FAILED")
     for i=1,#f do io.stderr:write(f[i]) end
     print()
   else print(" OK") end
-  self.failures = nil
+  if self.verbose and (#s > 0) then
+    for i=1,#s do io.stderr:write(s[i]) end
+    print()
+  end
+  self.failures,self.successes = nil,nil
 end
 
 local eq = function(self,x,y)
-  if (x == y) or tablex.deepcompare(x,y) then
-    printf(".")
-  else fail_eq(self,x,y) end
+  local ok = (x == y) or tablex.deepcompare(x,y)
+  ;(ok and pass_eq or fail_eq)(self,x,y)
 end
 
 local seq = function(self,x,y) -- list-sets
-  if tablex.compare_no_order(x,y) then
-    printf(".")
-  else fail_eq(self,x,y) end
+  local ok = tablex.compare_no_order(x,y)
+  ;(ok and pass_eq or fail_eq)(self,x,y)
 end
+
+local is_true = function(self,x)
+  (x and pass_assertion or fail_assertion)(self)
+end
+
+local is_false = function(self,x)
+  (x and fail_assertion or pass_assertion)(self)
 end
 
 local methods = {
@@ -54,10 +95,12 @@ local methods = {
   done = done,
   eq = eq,
   seq = seq,
+  yes = is_true,
+  no = is_false,
 }
 
-local new = function()
-  return setmetatable({},{__index = methods})
+local new = function(verbose)
+  return setmetatable({verbose = verbose or false},{__index = methods})
 end
 
 return {
