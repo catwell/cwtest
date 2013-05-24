a189b6f3b66c7d2c5869290cfe6bbd01e620fbbc
Pierre Chapuis <catwell@archlinux.us>
2013-05-24 16:39:14 +0200

    seq: support non-tables (assume not equal)

--- cwtest.lua
+++ cwtest.lua
@@ -16,7 +16,7 @@ deepcompare = function(t1,t2)
   local ty2 = type(t2)
   if ty1 ~= ty2 then return false end
   -- non-table types can be directly compared
-  if ty1 ~= 'table' then return t1 == t2 end
+  if ty1 ~= "table" then return t1 == t2 end
   -- as well as tables which have the metamethod __eq
   local mt = getmetatable(t1)
   if mt and mt.__eq then return t1 == t2 end
@@ -34,6 +34,8 @@ deepcompare = function(t1,t2)
 end
 
 local compare_no_order = function(t1,t2)
+  -- non-table types are considered *never* equal here
+  if (type(t1) ~= "table") or (type(t2) ~= "table") then return false end
   if #t1 ~= #t2 then return false end
   local visited = {}
   for i = 1,#t1 do
